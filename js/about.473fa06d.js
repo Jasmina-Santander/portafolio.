8e47ae33bdacbaab0a5862a2d9b26fec43bdfad2
Jasmina Santander <jasssantander@gmail.com>
2022-07-01 01:46:56 -0400

    gh-pages commit

---
+++ js/about.473fa06d.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkportafoliovue"]=self["webpackChunkportafoliovue"]||[]).push([[443],{381:function(n,u,t){t.r(u),t.d(u,{default:function(){return l}});var a=t(252);const e={class:"about"},o=(0,a._)("h1",null,"This is an about page",-1),r=[o];function s(n,u){return(0,a.wg)(),(0,a.iD)("div",e,r)}var i=t(744);const c={},f=(0,i.Z)(c,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.473fa06d.js.map
